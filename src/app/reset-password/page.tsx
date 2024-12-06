71d9f211af6e63621d12a9b4af0794f33e1e1f73
Aniket Patil <aniketpatil6400@gmail.com>
2024-12-06 21:04:40 +0530

    bugfix: fixed the supertoken issue

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,7 +1,40 @@
 'use client';
 import { ResetPasswordUsingToken } from 'supertokens-auth-react/recipe/emailpassword/prebuiltui';
+import { useEffect, useState } from 'react';
+import SuperTokens from 'supertokens-auth-react';
+import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
+
+const appName = process.env.NEXT_PUBLIC_APPNAME;
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
+const websiteDomain = process.env.NEXT_PUBLIC_WEBSITE_DOMAIN
+const apiBasePath = process.env.NEXT_PUBLIC_API_BASE_PATH;
 
 export default function Auth() {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+
+        SuperTokens.init({
+            appInfo: {
+                appName: `${appName}`,
+                apiDomain: `${apiDomain}`,
+                websiteDomain: `${websiteDomain}`,
+                apiBasePath: `${apiBasePath}`
+            },
+            recipeList: [
+                EmailPassword.init({
+                    resetPasswordUsingTokenFeature: {
+                        disableDefaultUI: true,
+                    },
+                }),
+            ],
+        });
+    }, []);
+
+    if (!isClient) {
+        return null;
+    }
     return (
         <div>
             <ResetPasswordUsingToken />
--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,7 +1,41 @@
 'use client';
 import { EmailVerification } from 'supertokens-auth-react/recipe/emailverification/prebuiltui';
+import { useEffect, useState } from 'react';
+import SuperTokens from 'supertokens-auth-react';
+import * as CustomEmailVerification from 'supertokens-auth-react/recipe/emailverification';
+import Session from 'supertokens-auth-react/recipe/session';
+
+const appName = process.env.NEXT_PUBLIC_APPNAME;
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
+const websiteDomain = process.env.NEXT_PUBLIC_WEBSITE_DOMAIN
+const apiBasePath = process.env.NEXT_PUBLIC_API_BASE_PATH;
 
 export default function Auth() {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+
+        SuperTokens.init({
+            appInfo: {
+                appName: `${appName}`,
+                apiDomain: `${apiDomain}`,
+                websiteDomain: `${websiteDomain}`,
+                apiBasePath: `${apiBasePath}`
+            },
+            recipeList: [
+                CustomEmailVerification.init({
+                    mode: 'REQUIRED',
+                }),
+                Session.init(),
+            ],
+        });
+    }, []);
+
+    if (!isClient) {
+        return null;
+    }
+
     return (
         <div>
             <EmailVerification />
