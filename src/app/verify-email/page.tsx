c79978f7ad8986839435e5bc5649cf3c808dd1a7
anas <anas.khan@reveation.io>
2025-01-15 15:41:47 +0530

    add: redirection verify-email to the app

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -7,38 +7,56 @@ import Session from 'supertokens-auth-react/recipe/session';
 
 const appName = process.env.NEXT_PUBLIC_APPNAME;
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
-const websiteDomain = process.env.NEXT_PUBLIC_WEBSITE_DOMAIN
+const websiteDomain = process.env.NEXT_PUBLIC_WEBSITE_DOMAIN;
 const apiBasePath = process.env.NEXT_PUBLIC_API_BASE_PATH;
+const loginRedirectUrl =
+  process.env.NEXT_PUBLIC_REDIRECT_TO_LOGIN || 'your-schema://login';
 
 export default function Auth() {
-    const [isClient, setIsClient] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+  const [emailVerified, setEmailVerified] = useState(false);
 
-    useEffect(() => {
-        setIsClient(true);
+  useEffect(() => {
+    setIsClient(true);
 
-        SuperTokens.init({
-            appInfo: {
-                appName: `${appName}`,
-                apiDomain: `${apiDomain}`,
-                websiteDomain: `${websiteDomain}`,
-                apiBasePath: `${apiBasePath}`
-            },
-            recipeList: [
-                CustomEmailVerification.init({
-                    mode: 'REQUIRED',
-                }),
-                Session.init(),
-            ],
-        });
-    }, []);
+    SuperTokens.init({
+      appInfo: {
+        appName: `${appName}`,
+        apiDomain: `${apiDomain}`,
+        websiteDomain: `${websiteDomain}`,
+        apiBasePath: `${apiBasePath}`,
+      },
+      recipeList: [
+        CustomEmailVerification.init({
+          mode: 'REQUIRED',
+          onHandleEvent: (event) => {
+            if (event.action === 'EMAIL_VERIFIED_SUCCESSFUL') {
+              setEmailVerified(true);
+              setTimeout(() => {
+                window.location.href = loginRedirectUrl;
+              }, 2000);
+            }
+          },
+        }),
+        Session.init(),
+      ],
+    });
+  }, []);
 
-    if (!isClient) {
-        return null;
-    }
+  if (!isClient) {
+    return null;
+  }
 
-    return (
+  return (
+    <div>
+      {!emailVerified ? (
+        <EmailVerification />
+      ) : (
         <div>
-            <EmailVerification />
+          <h2>Email verified successfully!</h2>
+          <p>Redirecting to login...</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
