667675f686672feb384be36e6d270dcfd324980f
anas <anas.khan@reveation.io>
2025-01-15 17:07:39 +0530

    add: redirection reset-password to the app

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -6,38 +6,57 @@ import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
 
 const appName = process.env.NEXT_PUBLIC_APPNAME;
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
-const websiteDomain = process.env.NEXT_PUBLIC_WEBSITE_DOMAIN
+const websiteDomain = process.env.NEXT_PUBLIC_WEBSITE_DOMAIN;
 const apiBasePath = process.env.NEXT_PUBLIC_API_BASE_PATH;
+const loginRedirectUrl =
+  process.env.NEXT_PUBLIC_REDIRECT_TO_LOGIN || 'your-schema://login';
 
 export default function Auth() {
-    const [isClient, setIsClient] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+  const [passwordResetSuccess, setPasswordResetSuccess] = useState(false);
 
-    useEffect(() => {
-        setIsClient(true);
+  useEffect(() => {
+    setIsClient(true);
 
-        SuperTokens.init({
-            appInfo: {
-                appName: `${appName}`,
-                apiDomain: `${apiDomain}`,
-                websiteDomain: `${websiteDomain}`,
-                apiBasePath: `${apiBasePath}`
-            },
-            recipeList: [
-                EmailPassword.init({
-                    resetPasswordUsingTokenFeature: {
-                        disableDefaultUI: true,
-                    },
-                }),
-            ],
-        });
-    }, []);
+    SuperTokens.init({
+      appInfo: {
+        appName: `${appName}`,
+        apiDomain: `${apiDomain}`,
+        websiteDomain: `${websiteDomain}`,
+        apiBasePath: `${apiBasePath}`,
+      },
 
-    if (!isClient) {
-        return null;
-    }
-    return (
+      recipeList: [
+        EmailPassword.init({
+          resetPasswordUsingTokenFeature: {
+            disableDefaultUI: true,
+          },
+          onHandleEvent: (event) => {
+            if (event.action === 'PASSWORD_RESET_SUCCESSFUL') {
+              setPasswordResetSuccess(true);
+              setTimeout(() => {
+                window.location.href = loginRedirectUrl;
+              }, 2000);
+            }
+          },
+        }),
+      ],
+    });
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+  return (
+    <div>
+      {!passwordResetSuccess ? (
+        <ResetPasswordUsingToken />
+      ) : (
         <div>
-            <ResetPasswordUsingToken />
+          <h2>Password reset successfully!</h2>
+          <p>Redirecting to login...</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
